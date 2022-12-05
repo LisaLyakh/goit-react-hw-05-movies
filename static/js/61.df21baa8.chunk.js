7f75fd1bcc8ccf7a2a7894442fd68d0a3214c1c6
LisaLyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-12-05 08:09:58 +0000

    Deploying to gh-pages from @ LisaLyakh/goit-react-hw-05-movies@4b1bf8a0b5f3202fcfd81c38745acaecfb711b7a 🚀

---
+++ static/js/61.df21baa8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[61],{61:function(e,r,t){t.r(r),t.d(r,{default:function(){return o}});var n=t(731),a=t(184);function o(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:" 404 Sorry, the page was not found"}),(0,a.jsx)(n.rU,{to:"/",children:"To main page"})]})}}}]);
+//# sourceMappingURL=61.df21baa8.chunk.js.map
